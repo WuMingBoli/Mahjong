2670d539cd4c9799b9c0478600a921bae99a43b1
WuMingBoli <39850609+WuMingBoli@users.noreply.github.com>
2026-04-17 10:50:36 +0800

    更新

---
+++ assets/level76/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/level76",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/level76', 'chunks:///_virtual/level76'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
